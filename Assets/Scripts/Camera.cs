81913af4525f3b8c848bf51d35038d38183d7057
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2016-10-05 16:14:31 +1100

    basic sphere spawns above terrain, camera follows strictly

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace COMP30019.Project2
+{
+    public class Camera : MonoBehaviour
+    {
+
+        public GameObject player;
+
+        public Vector3 offset;
+        public Vector3 rotation = new Vector3 { x = 45, y = 0, z = 0 };
+
+        void Start()
+        {
+        }
+
+        void LateUpdate()
+        {
+            transform.position = player.transform.position + offset;
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+    }
+}
