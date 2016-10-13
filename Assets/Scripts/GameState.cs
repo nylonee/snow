1eab571854b7cd9dda4656f0f4288981589a8857
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2016-10-13 14:52:30 +1100

    Added main menu

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace COMP30019.Project2
+{
+    public class GameState : MonoBehaviour
+    {
+        public void loadGame()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        }
+
+        public void endGame()
+        {
+            Application.Quit();
+        }
+
+        public void mainMenu()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+    }
+
+}
