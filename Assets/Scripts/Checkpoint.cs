0ab8b9ac18314db42bddacc7be53e5fd89a1c5f0
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2016-10-17 04:02:57 +1100

    added high score to gameover scene

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -7,10 +7,12 @@ namespace COMP30019.Project2
     {
         public float distPastCheckpointToFail = 5.0f;
         private GameObject player;
+        private TimeUpdate score;
 
         void Start()
         {
             player = GameObject.FindGameObjectWithTag("Player");
+            score = GameObject.FindGameObjectWithTag("Time").GetComponent<TimeUpdate>();
         }
 
         void Update()
@@ -19,6 +21,7 @@ namespace COMP30019.Project2
 
             if (player.transform.position.z > transform.position.z + distPastCheckpointToFail)
             {
+                if(PlayerPrefs.GetFloat("highscore") > score.getTime()) PlayerPrefs.SetFloat("highscore", score.getTime());
                 UnityEngine.SceneManagement.SceneManager.LoadScene(2);
             }
         }
