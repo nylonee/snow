aa29386475e0eb3ea6af8a9d0274bad8b22db88c
jgrooby <jgrooby@student.unimelb.edu.au>
2016-10-18 18:51:04 +1100

    handle player falling over

---
+++ Assets/Scripts/PlayerFallingOverHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+namespace COMP30019.Project2
+{
+    public class PlayerFallingOverHandler : MonoBehaviour
+    {
+        private bool isTouchingGround = false;
+        private float t;
+
+        void Update()
+        {
+            if (isTouchingGround)
+                if (t < 0.0f)
+                    SceneManager.LoadScene(2);
+                else
+                    t -= Time.deltaTime;
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.gameObject.tag == "Terrain")
+            {
+                isTouchingGround = true;
+                t = 3.0f;
+            }
+        }
+
+        void OnCollisionExit(Collision collision)
+        {
+            if (collision.collider.gameObject.tag == "Terrain")
+                isTouchingGround = false;
+        }
+    }
+}
